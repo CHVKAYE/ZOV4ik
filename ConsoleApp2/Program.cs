bfb8815ada8785fe69d528c211a87733500e1807
chiro <chiro@DESKTOP-GRQ1BIO>
2024-03-07 20:03:07 +0300

    Я поменял значение переменных a и b

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        int a = 6;
+        int b = 7;
+        Console.WriteLine(a + b);
+    }
+}
